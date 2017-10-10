de37cae2659b586de8e6c57d21ea5ebeb3aa6a56
brunadafonseca <brunadafonseca@gmail.com>
2017-10-10 12:08:59 +0200

    One-to-Many: Models

---
+++ db/migrate/20171010092020_create_events.rb
@@ -0,0 +1,19 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.text :description
+      t.string :location
+      t.decimal :price
+      t.integer :capacity
+      t.boolean :includes_food
+      t.string :includes_drinks
+      t.string :boolean
+      t.datetime :starts_at
+      t.datetime :ends_at
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
