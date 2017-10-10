e432735bf5db891627c5a0e42e5f093f1babbb24
brunadafonseca <brunadafonseca@gmail.com>
2017-10-10 12:16:49 +0200

    One-to-Many: Controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,8 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :description, presence: true, length: { maximum: 500 }
+  validates :price, default: 0
+  validates :includes_food, default: false
+  validates :includes_drinks, default: false
+  validates :active, default: true
 end
