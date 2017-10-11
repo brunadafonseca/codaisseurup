cdd291495cb9d0d74c2d6ce1a6765164ec357203
brunadafonseca <brunadafonseca@gmail.com>
2017-10-11 21:02:29 +0200

    Install FactoryGirl, faker and define factories

--- Gemfile
+++ Gemfile
@@ -45,6 +45,10 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 
   gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
+
+  gem 'factory_girl_rails'
+
+  gem 'faker'
 end
 
 group :test do
---
+++ spec/factories/events.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :room do
+    name      { Faker::Lorem.sentence(1) }
+    description       { Faker::Lorem.sentence(40) }
+    location          { Faker::Address.city }
+    includes_food     true
+    includes_drinks   true
+    price             { Faker::Commerce.price }
+    user              { build(:user) }
+
+    trait :active do
+      active true
+    end
+
+    trait :inactive do
+      active false
+    end
+  end
+end
