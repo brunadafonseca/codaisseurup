e3727a753fcf2cfe1e36d05c2f0cad2927eae400
brunadafonseca <brunadafonseca@gmail.com>
2017-10-11 20:13:36 +0200

    Add user to events

--- db/migrate/20171010092020_create_events.rb
+++ db/migrate/20171010092020_create_events.rb
@@ -7,8 +7,7 @@ class CreateEvents < ActiveRecord::Migration[5.1]
       t.decimal :price
       t.integer :capacity
       t.boolean :includes_food
-      t.string :includes_drinks
-      t.string :boolean
+      t.boolean :includes_drinks
       t.datetime :starts_at
       t.datetime :ends_at
       t.boolean :active
