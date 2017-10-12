0ad442c7deb43f317ba62b7b2d7ba47853ee9230
brunadafonseca <brunadafonseca@gmail.com>
2017-10-12 13:22:44 +0200

    Create Photo: Model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,7 @@ class EventsController < ApplicationController
 
   def show
     @categories = @event.categories
+    @photos = @event.photos
   end
 
   def new
@@ -18,17 +19,31 @@ class EventsController < ApplicationController
     @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to @event, notice: "Event created"
+      image_params.each do |image|
+        @event.photos.create(image: image)
+      end
+
+      redirect_to edit_event_path(@event), notice: "Event created"
     else
       render :new
     end
   end
 
-  def edit; end
+  def edit
+    if current_user.id == @event.user.id
+      @photos = @event.photos
+    else
+      redirect_to root_path, notice: "You don't have permission."
+    end
+  end
 
   def update
     if @event.update(event_params)
-      redirect_to @event, notice: "Event updated"
+      image_params.each do |image|
+        @event.photos.create(image: image)
+      end
+
+      redirect_to edit_event_path(@event), notice: "Event updated"
     else
       render :edit
     end
@@ -46,5 +61,9 @@ class EventsController < ApplicationController
       .permit(
         :name, :capacity, :description, :location, :includes_food, :includes_drinks, :price, :active, category_ids: []
       )
+  end
+
+  def image_params
+    params[:images].present? ? params.require(:images) : []
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Photo.destroy_all
+Category.destroy_all
+Event.destroy_all
+Profile.destroy_all
 User.destroy_all
 
 music = Category.create(name: "Music")
@@ -13,6 +17,18 @@ bruna = User.create(email: "bruna@codaisseurup.com", password: "123456")
 bart = User.create(email: "bart@codaisseurup.com", password: "abc123")
 eduardo = User.create(email: "eduardo@codaisseurup.com", password: "ab1234")
 
-Event.create(name: "Vondelpark Open Air", description: "bla bla bla", location: "Vondelpark", price: 0, user: bruna, categories: [music, food_and_drinks, family, kids])
-Event.create(name: "Bart's Birthday", description: "Bart turns 30", location: "Amsterdam", price: 0, user: bart, categories: [music, food_and_drinks])
-Event.create(name: "Learn to Code", description: "bla bla bla", location: "Codaisseur", price: 0, user: eduardo, categories: [tech])
+event1 = Event.create!(name: "Vondelpark Open Air", description: "bla bla bla", location: "Vondelpark", price: 0, user: bruna)
+event2 = Event.create!(name: "Bart's Birthday", description: "Bart turns 30", location: "Amsterdam", price: 0, user: bart)
+event3 = Event.create!(name: "Learn to Code", description: "bla bla bla", location: "Codaisseur", price: 0, user: eduardo)
+
+photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800640/pexels-photo-325521_dy9ch0.jpg", event: event1)
+photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800640/pexels-photo-325521_dy9ch0.jpg", event: event1)
+photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800640/pexels-photo-325521_dy9ch0.jpg", event: event1)
+
+photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800650/pexels-photo-276926_cohpwn.jpg", event: event2)
+photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800650/pexels-photo-276926_cohpwn.jpg", event: event2)
+photo6 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800650/pexels-photo-276926_cohpwn.jpg", event: event2)
+
+photo7 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800640/pexels-photo-325521_dy9ch0.jpg", event: event3)
+photo8 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800640/pexels-photo-325521_dy9ch0.jpg", event: event3)
+photo9 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dyyxiefx5/image/upload/v1507800640/pexels-photo-325521_dy9ch0.jpg", event: event3)
